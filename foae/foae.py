92ede660ff3cb67babc6ed8e22271489aeb92a5f
Gustav <gustav-git@pm.me>
2022-08-17 20:59:24 +0200

    Simplify nested decorator for loop

--- foae/foae.py
+++ foae/foae.py
@@ -18,64 +18,68 @@ class Foae:
         source = inspect.getsource(module)
         src_ast = ast.parse(source)
 
+        routes = {}
         for node in ast.walk(src_ast):
             if not isinstance(node, ast.FunctionDef):
                 continue
 
+            def is_flask_route(decorator: ast.Call) -> bool:
+                return isinstance(decorator, ast.Call) and decorator.func.attr == 'route' and isinstance(decorator.args[0], ast.Str)
+
+            decorator = next((decorator for decorator in node.decorator_list if is_flask_route(decorator)), None)
+            if not decorator:
+                continue
+
             definition = {}
-            rule = ''
-            for decorator in node.decorator_list:
-                # Verify that the decorator is @app.route
-                if not isinstance(decorator, ast.Call) or decorator.func.attr != 'route':
+
+            # Extract the supported HTTP methods
+            for keyword in decorator.keywords:
+                if keyword.arg != 'methods':
                     continue
 
-                # Verify that rule exists
-                if not isinstance(decorator.args[0], ast.Str):
-                    continue
+                for value in keyword.value.elts:
+                    http_method = value.s.lower()
+                    definition[http_method] = {'tags': [node.name]}
 
-                # Extract the supported HTTP methods
-                for keyword in decorator.keywords:
-                    if keyword.arg != 'methods':
-                        continue
-
-                    for value in keyword.value.elts:
-                        http_method = value.s.lower()
-                        definition[http_method] = {'tags': [node.name]}
-
-                if not definition:
-                    definition['get'] = {
-                        'tags': [node.name],
-                        'response': {
-                            200: {
-                                'description': 'Success'
-                            }
-                        }
+            if not definition:
+                definition['get'] = { 'tags': [node.name] }
+            
+            for _, d in definition.items():
+                d['response'] = {
+                    200: {
+                        'description': 'Success'
                     }
-
-                # Extract the path parameters
-                rule = decorator.args[0].s
-                path_params = [part[1:-1] for part in rule.split('/') if part.startswith('<')]
-                if not path_params:
-                    continue
-                
-                # Get type hint of each parameter
-                type_hints = {
-                    param.arg: self._map_type(param.annotation.id)
-                    for param in node.args.args + node.args.kwonlyargs + node.args.kw_defaults 
-                    if isinstance(param.annotation, ast.Name)
                 }
 
-                definition['parameters'] = [{
-                    'name': param,
-                    'in': 'path',
-                    'required': True,
-                    'type': type_hints[param]
-                } for param in path_params]
+            # Store the route definition
+            rule = decorator.args[0].s
+            rule = rule.replace('<', '{').replace('>', '}')
+            routes[rule] = definition
+
+            # Extract the path parameters
+            path_params = [part[1:-1] for part in rule.split('/') if part.startswith('{')]
+            if not path_params:
+                continue
+            
+            # Get type hint of each parameter
+            type_hints = {
+                param.arg: self._map_type(param.annotation.id)
+                for param in node.args.args + node.args.kwonlyargs + node.args.kw_defaults 
+                if isinstance(param.annotation, ast.Name)
+            }
+
+            definition['parameters'] = [{
+                'name': param,
+                'in': 'path',
+                'required': True,
+                'type': type_hints[param]
+            } for param in path_params]
 
             if not definition:
                 continue
             
-            rule = rule.replace('<', '{').replace('>', '}')
+        # Store routes
+        for rule, definition in routes.items():
             self.spec['paths'][rule] = definition
 
     def _map_type(self, t: type) -> str:
@@ -92,7 +96,6 @@ class Foae:
         return 'string'
 
     def export_as(self, extension: str) -> None:
-        print(self.spec)
         file_name = 'openapi.{}'.format(extension)
         with open(file_name, 'w') as f:
             if extension == 'yaml':
